221dda68a476699143ea3ab615817208c97c4757
jamespixelpusher <james@pixelpusher.ca>
2025-09-16 10:18:06 -0400

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,9 @@ import Link from "next/link";
 
 export default function App() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <Link
-        href="/chat"
-        className="px-6 py-3 rounded-md bg-white text-black font-medium"
-      >
-        Start
+    <div className="relative w-screen h-screen">
+      <Link href="/chat" className="absolute left-1/2 -translate-x-1/2" style={{ bottom: "10%" }} aria-label="Start">
+        <img src="/start_button.svg" alt="Start" />
       </Link>
     </div>
   );
